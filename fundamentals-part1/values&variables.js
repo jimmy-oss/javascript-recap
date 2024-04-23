6d255555e3018a6209e2bed71b1e985fa35c6167
jimmy-oss <jimmynjonge51@gmail.com>
2024-04-23 17:01:15 +0300

    recaped values&variables

---
+++ fundamentals-part1/values&variables.js
@@ -0,0 +1,17 @@
+let js = "jimmy is an awesome developer!";
+console.log(50 + 20 + 70 - 10);
+console.log("jimmy");
+console.log(23);
+
+let firstName = "jimmy";
+console.log(firstName);
+
+let last_name_person = "john";
+console.log(last_name_person);
+
+let person = "mike";
+console.log(person);
+
+let myFirstJob = 'programmer';
+let myCurrentJob = 'teacher';
+console.log(myFirstJob);
