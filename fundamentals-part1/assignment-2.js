a651422c8b58e85c4b761dd35782424bb20be279
jimmy-oss <jimmynjonge51@gmail.com>
2024-04-23 18:33:13 +0300

    data-types assignment

---
+++ fundamentals-part1/assignment-2.js
@@ -0,0 +1,13 @@
+let isIsland = true;
+
+let language;
+let myAge = 23;
+
+let country = "kenya";
+let population = '50.03 million';
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+console.log(typeof myAge);
