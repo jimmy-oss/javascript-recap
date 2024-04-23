1048a8ca91e2755a04bcdcec4aa500691753a428
jimmy-oss <jimmynjonge51@gmail.com>
2024-04-23 17:54:58 +0300

    data-types fundamentals

---
+++ fundamentals-part1/data-types.js
@@ -0,0 +1,29 @@
+/* The 7 primitive data types
+
+ 1.Number: floating point numbers used for decimals. 
+   and integers let age = 23;
+
+ 2.String: Sequence of characters used for text. 
+   let firstName = "jimmy";
+
+ 3.Boolean: Logical type that can only be true or 
+   false used for taking decisions.
+   let fullAge = true;
+
+ 4.Undefined: Value taken by a variable that is not 
+  yet defined('empty value').
+  let children;
+
+ 5.Null: Also means 'empty value'.
+
+ 6.Symbol(ES2015): Value that is unique and cannot 
+   be changed [Not Useful For Now].
+
+ 7.BigInt: Larger integers than the Number type can 
+   hold.
+  
+   Javascript has dynamic typing: We do not have to 
+   manually define the data type of the value stored 
+   in a variable.Instead, data types are determined 
+   automatically.
+*/
