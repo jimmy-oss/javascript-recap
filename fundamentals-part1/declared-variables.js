f652e99e59646b6098cef110bbbce71a38ba85db
jimmy-oss <jimmynjonge51@gmail.com>
2024-05-14 14:02:23 +0300

    declared-variables

--- fundamentals-part1/declared-variables.js
+++ fundamentals-part1/declared-variables.js
@@ -4,7 +4,12 @@ console.log(age);
 
 const birthYear = 1991;
 console.log(birthYear);
+// birthYear = 1992; it will bring an error because 
+// const is not for immutable like let 
 
 // let is used if the declared value will change which
 // is immutable as compared to const the value which has
 // been declared cannot change according to the js world
+// var is a legacy world used in Es6 in the older days
+// so its recommended to just stick with var or const 
+// in our current generations
