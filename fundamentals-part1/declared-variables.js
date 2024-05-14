2759cb06091cb6c8f8219dce26bbba24456fafd6
jimmy-oss <jimmynjonge51@gmail.com>
2024-05-14 14:03:43 +0300

    declared-variables

--- fundamentals-part1/declared-variables.js
+++ fundamentals-part1/declared-variables.js
@@ -8,7 +8,7 @@ console.log(birthYear);
 // const is not for immutable like let 
 
 // let is used if the declared value will change which
-// is immutable as compared to const the value which has
+// is around to mutate as compared to const the value which has
 // been declared cannot change according to the js world
 // var is a legacy world used in Es6 in the older days
 // so its recommended to just stick with var or const 
