38e6fbc5e18288c3f7fa87d7a756bc9acc3b3c6e
jimmy-oss <jimmynjonge51@gmail.com>
2024-05-14 13:59:19 +0300

    declared-variables

---
+++ fundamentals-part1/declared-variables.js
@@ -0,0 +1,10 @@
+let age = 30;
+age = 31;
+console.log(age);
+
+const birthYear = 1991;
+console.log(birthYear);
+
+// let is used if the declared value will change which
+// is immutable as compared to const the value which has
+// been declared cannot change according to the js world
